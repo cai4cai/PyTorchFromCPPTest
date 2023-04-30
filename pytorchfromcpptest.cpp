6bc23d23e3782e3651db23a625b1d9b8bfa464ca
Tom Vercauteren <tom.vercauteren@kcl.ac.uk>
2023-04-30 17:16:40 +0100

    Adding basic CI

--- pytorchfromcpptest.cpp
+++ pytorchfromcpptest.cpp
@@ -1,9 +1,15 @@
+// Copyright 2023 Tom Vercauteren. All rights reserved.
+//
+// This software is licensed under the Apache 2 License.
+// See the LICENSE file for details.
+
 #include <torch/torch.h>
+
 #include <iostream>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
   try {
-    std::cout<<"Starting test."<<std::endl;
+    std::cout << "Starting test." << std::endl;
     torch::Tensor tensor = torch::rand({2, 3});
     std::cout << "Random 2x3 tensor:" << std::endl << tensor << std::endl;
   } catch (const std::exception& e) {
